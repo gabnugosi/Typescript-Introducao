6a89a577367b06f1f55c99d698bab133b298b3cd
Gabriel Nunes <gabriel-g-silva@openlabs.com.br>
2019-09-13 14:17:02 -0300

    Capturando os dados do formulário

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,6 @@
-const negociacao = new Negociacao(new Date(), 1, 100);
-console.log(negociacao.quantidade);
+/*const negociacao = new Negociacao(new Date(), 1, 100);
+console.log(negociacao.quantidade);*/
+const controller = new NegociacaoController();
+document
+    .querySelector('.form')
+    .addEventListener('submt', controller.adiciona.bind(controller));
